e0effb5394ca5012e99fbd03f64dda3b56af4692
17Moons <vaibhav.singh.cd.eee20@itbhu.ac.in>
2022-01-22 19:21:17 +0530

    ok i was wrong one more commit it is

--- src/layouts/register/createTeam.js
+++ src/layouts/register/createTeam.js
@@ -45,7 +45,7 @@ function CreateTeam() {
             </MDBox> */}
               <Tilt className="Tilt" options={{ max : 25, scale: 1.02}}>
                     <MDBox pt={3} pb={2} >
-                        <MDBox  mb={1} display="flex" flexDirection="column" sx={{ marginLeft: "22%",}} >
+                        <MDBox  mb={1} display="flex" flexDirection="column" sx={{ marginLeft: "20%", marginRight: "10%",}} >
                         <a href="team/:team_name" >
                <button type="button" className="btn btn-outline-primary btn-lg btn-block">
                 Create the team!
